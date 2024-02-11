49ba31f4c2eca0efc956bf1041e16f7783c0a99a
KremerM <44829599+KremerM@users.noreply.github.com>
2024-02-11 17:38:39 -0500

    Implement cover url and add book
    feat: covers from urls, add book to reader store

--- src/components/reader/Home.vue
+++ src/components/reader/Home.vue
@@ -11,7 +11,7 @@
         <div v-for="(info, index) in bookList" :key="index">
           <!-- 主体 -->
           <el-card @click="readerBook(info)" ref="card" shadow="hover" class="box-card" :body-style="{ padding: '0px' }">
-            <el-image :lazy="true" :src="'data:image/png;base64,' + info.coverBase64" fit="fill" class="el-image">
+            <el-image :lazy="true" :src="info.coverPath" fit="fill" class="el-image">
               <template #error>
                 <div class="image-slot">
                   <el-icon><icon-picture /></el-icon>
--- src/components/reader/utils/stores.js
+++ src/components/reader/utils/stores.js
@@ -29,7 +29,22 @@ export const useReaderStore = defineStore('reader', {
         this.bookList[index] = info
       }
     },
-    addBook() {},
+    addBook({ pubDate, author, language, title, coverPath, url}) {
+      const book = {
+        date: pubDate,
+        creator: author,
+        UUID: "DB108833-1D43-4711-A2D0-240E07B37352",
+        language: language,
+        title: title,
+        coverPath: coverPath,
+        url: url,
+        bgColorFromCover: "#076184",
+        size: 435507,
+        from: "url"
+      }
+      console.log("push book")
+      this.bookList.push(book)
+    },
     delBook(id) {
       const index = this.bookList.findIndex((item) => id === item.id)
       if (index > -1) {
